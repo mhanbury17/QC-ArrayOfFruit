1b8c636b5322ec37ddbb956ff10784a72b48cd85
mhanbury17 <48768769+mhanbury17@users.noreply.github.com>
2019-03-20 13:49:18 -0400

    Add project files.

---
+++ ArrayOfFruit/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayOfFruit
+{
+    class Program
+    {
+        public enum Fruit
+        {
+            none,
+            Orange,
+            Apple,
+            Watermelon,
+            Pineapple,
+            Necturine,
+            Grapes,
+        }
+        static void Main(string[] args)
+        {
+            Fruit[] fruits = new Fruit[3];
+            string userInput;
+
+            for (int i = 0; i < fruits.Count(); i++)
+            {
+                Console.Write($"Enter fruit {i+1}: ");
+                userInput = Console.ReadLine();
+                Enum.TryParse(userInput, out fruits[i]);
+            }
+            Console.WriteLine();
+
+            foreach (Fruit fruit in fruits)
+            {
+                Console.WriteLine(fruit);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
+        }
+    }
+}
